96e63b8a3b9691aafda4be1004032b7eecd423fa
Bruce Grugett <bgrugett@gmail.com>
2017-03-30 15:30:25 -0400

    end of Thurday

--- browser/react/components/Sidebar.js
+++ browser/react/components/Sidebar.js
@@ -20,12 +20,23 @@ const Sidebar = (props) => {
             <hr />
       <section>
         <h4 className="text-muted">PLAYLISTS</h4>
+        <ul className="list-unstyled">
+        <li className="playlist-item menu-item">
+        <Link to="FILL_ME_IN">some playlist</Link>
+        </li>
+        <li className="playlist-item menu-item">
+          <Link to="WHERE_TO_GO">another playlist</Link>
+        </li>
+      </ul>
         <h4>
           <Link className="btn btn-primary btn-block" to="/NewPlaylist">
             <span className="glyphicon glyphicon-plus"></span> PLAYLIST
           </Link>
         </h4>
+
       </section>
+      <hr />
+
     </sidebar>
   );
 }
--- browser/react/containers/NewPlaylistContainer.js
+++ browser/react/containers/NewPlaylistContainer.js
@@ -4,58 +4,59 @@ import axios from 'axios';
 
 
 class NewPlaylistContainer extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            inputValue: '',
-            disabled: false
-        };
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.disabled = this.disabled.bind(this)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      disabled: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.disabled = this.disabled.bind(this)
+  }
 
-    handleChange (evt) {
-      const value = evt.target.value;
-          this.setState({
-              inputValue: value,
-              disabled: this.disabled(value.length)
-          });
-    }
+  handleChange(evt) {
+    const value = evt.target.value;
+    this.setState({
+      inputValue: value,
+      disabled: this.disabled(value.length)
+    });
+  }
 
-    handleSubmit (evt) {
-      evt.preventDefault();
-      // const value = evt.target.playListInput.value;
-      // console.log('event value ', value);
-      this.setState({
-          inputValue: ''
+  handleSubmit(evt) {
+    evt.preventDefault();
+    // const value = evt.target.playListInput.value;
+    // console.log('event value ', value);
+    axios.post('/api/playlists', {name: this.state.inputValue })
+      .then(res => res.data)
+      .then(result => {
+        console.log(result) // response json from the server!
       });
-    }
 
-    disabled(val){
-      if(val> 16 || val<= 0) return true;
-      else return false;
-    }
+    this.setState({
+      inputValue: ''
+    });
+  }
 
-    axios.post('/api/route/to/post/to', { /** req.body contents go here! */ })
-  .then(res => res.data)
-  .then(result => {
-    console.log(result) // response json from the server!
-  });
+  disabled(val) {
+    if (val > 16 || val <= 0) return true;
+    else return false;
+  }
 
-    render () {
-        // const inputValue = this.state.inputValue;
-      console.log('handleSubmit  ', this.handleSubmit);
-        return (
-            <div>
-              { this.state &&
-                <NewPlaylist handleSubmit= {this.handleSubmit}
-                inputValue={this.state.inputValue} onChange={this.handleChange}
-                disabled={this.state.disabled} />
-              }
-            </div>
-        );
-    }
+
+render() {
+  // const inputValue = this.state.inputValue;
+  console.log('handleSubmit  ', this.handleSubmit);
+  return (
+    <div>
+      {this.state &&
+        <NewPlaylist handleSubmit={this.handleSubmit}
+          inputValue={this.state.inputValue} onChange={this.handleChange}
+          disabled={this.state.disabled} />
+      }
+    </div>
+  );
+}
 }
 
 export default NewPlaylistContainer;
